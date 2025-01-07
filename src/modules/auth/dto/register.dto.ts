9008169eb06888e7fa9700324cf788519c176714
Vehan Hemsara <79366878+Hemsara@users.noreply.github.com>
2025-01-07 21:48:39 +0000

    refactor authentication module: replace DTOs for login and registration, remove unused files

---
+++ src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+
+
+export class RegisterDTO {
+
+    @IsString()
+    @IsNotEmpty()
+    firstName   : string;
+
+    @IsString()
+    @IsNotEmpty()
+    lastName    : string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+    email       : string;
+
+    @IsString()
+    @IsNotEmpty()
+    password    : string;
+}
